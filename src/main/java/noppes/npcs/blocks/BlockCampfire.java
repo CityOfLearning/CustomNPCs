34d25b010050e4970378bf30cbf20a5da892b78d
Dominic Amato <dominic.a.amato@gmail.com>
2017-03-22 01:14:58 -0500

    make campfire set spawn

--- src/main/java/noppes/npcs/blocks/BlockCampfire.java
+++ src/main/java/noppes/npcs/blocks/BlockCampfire.java
@@ -12,6 +12,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.world.World;
@@ -49,25 +50,38 @@ public class BlockCampfire extends BlockLightable {
 	@Override
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side,
 			float hitX, float hitY, float hitZ) {
-		ItemStack item = player.inventory.getCurrentItem();
-		if (item == null) {
-			return true;
-		}
-		if (((item.getItem() == Items.flint) || (item.getItem() == Items.flint_and_steel)) && (unlitBlock() == this)) {
-			if ((world.rand.nextInt(3) == 0) && (!world.isRemote)) {
-				super.onBlockActivated(world, pos, state, player, side, hitX, hitY, hitZ);
+		if (!world.isRemote) {
+			ItemStack item = player.inventory.getCurrentItem();
+			if (item == null) {
+				return true;
 			}
-			CustomNpcs.proxy.spawnParticle(EnumParticleTypes.SMOKE_LARGE, pos.getX() + 0.5F, pos.getY() + 0.5F,
-					pos.getZ() + 0.5F, 0.0D, 0.0D, 0.0D, 2.0F);
-			if (item.getItem() == Items.flint) {
-				NoppesUtilServer.consumeItemStack(1, player);
-			} else {
-				item.damageItem(1, player);
+			if (((item.getItem() == Items.flint) || (item.getItem() == Items.flint_and_steel))
+					&& (unlitBlock() == this)) {
+				if ((world.rand.nextInt(3) == 0) && (!world.isRemote)) {
+					super.onBlockActivated(world, pos, state, player, side, hitX, hitY, hitZ);
+				}
+				CustomNpcs.proxy.spawnParticle(EnumParticleTypes.SMOKE_LARGE, pos.getX() + 0.5F, pos.getY() + 0.5F,
+						pos.getZ() + 0.5F, 0.0D, 0.0D, 0.0D, 2.0F);
+				if (item.getItem() == Items.flint) {
+					NoppesUtilServer.consumeItemStack(1, player);
+				} else {
+					item.damageItem(1, player);
+				}
+				return true;
 			}
-			return true;
-		}
-		if ((item.getItem() == Item.getItemFromBlock(Blocks.sand)) && (litBlock() == this)) {
-			super.onBlockActivated(world, pos, state, player, side, hitX, hitY, hitZ);
+			if ((litBlock() == this)) {
+				if ((item.getItem() == Item.getItemFromBlock(Blocks.sand))) {
+					super.onBlockActivated(world, pos, state, player, side, hitX, hitY, hitZ);
+				} else if (item.getItem() == Items.stick) {
+					player.setSpawnPoint(player.getPosition(), true);
+					player.addChatComponentMessage(new ChatComponentTranslation("Set Spawn Point to: %s", pos));
+				}
+			}
+		} else {
+			if ((litBlock() == this) && (player.inventory.getCurrentItem() != null)
+					&& (player.inventory.getCurrentItem().getItem() == Items.stick)) {
+				player.setSpawnPoint(player.getPosition(), true);
+			}
 		}
 		return true;
 	}
@@ -75,6 +89,10 @@ public class BlockCampfire extends BlockLightable {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void randomDisplayTick(World world, BlockPos pos, IBlockState state, Random random) {
+		// if(world.getLightFor(EnumSkyBlock.SKY, pos) > 7){
+		//
+		// }
+
 		if (state.getBlock() != CustomItems.campfire) {
 			return;
 		}
